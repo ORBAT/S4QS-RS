86cbd5dec0fd4aad21902f6996940ac16e4ea7da
Tom Eklöf <tome@unity3d.com>
2015-02-18 10:50:48 -0500

    make Manifest take SQS message objects instead of URIs so it's easier to associate them with messages

--- test/manifest-uploader-spec.js
+++ test/manifest-uploader-spec.js
@@ -2,6 +2,8 @@
  * Created by teklof on 17.2.15.
  */
 
+
+var ut = require('../lib/utils');
 var mup = require('../lib/manifest-uploader');
 var tu = require('./test-utils');
 var _ = require('lodash');
@@ -12,11 +14,17 @@ require('mocha-sinon');
 var expect = chai.expect;
 var should = chai.should();
 
+var inspect = _.partialRight(util.inspect, {depth: 3});
+
 describe("Manifest uploader", function () {
 
   var manifBucket = "bukkit"
     , manifPrefix = "my-prefix/"
   ;
+
+  function newSQSMsg(n) {
+    return new tu.SQSMessage(n, "other-bukkit", "other-prefix/");
+  }
 
   function newManifest(mandatory, n, put, del) {
     var s3 = new tu.FakeS3(put, del)
@@ -59,7 +67,6 @@ describe("Manifest uploader", function () {
         return expect(m._upload()).to.be.fulfilled.then(function () {
           expect(putObject).to.have.been.calledOnce;
           var arg = putObject.firstCall.args[0];
-//          expect(putObject).to.have.been.calledWithMatch({Bucket: manifBucket, Key: m._key});
           expect(arg.Bucket).to.equal(manifBucket);
           expect(arg.Key).to.equal(m._key);
           expect(arg.ContentType).to.equal("application/json");
@@ -103,33 +110,66 @@ describe("Manifest uploader", function () {
     });
 
     it("should return correct length", function () {
-      var m = new mup.Manifest(true);
-      m._push("uri1");
-      m._push("uri2");
+      var m = new mup.Manifest(true)
+        , msgs = newSQSMsg(2).Messages
+        ;
+      m._push(msgs[0]);
+      m._push(msgs[1]);
       expect(m.length).to.equal(2);
     });
 
-    it("should _push new URIs", function () {
-      var m = new mup.Manifest(true);
-      expect(m._push("uri1")).to.equal(1);
-      expect(m.uris).to.deep.equal(["uri1"]);
+    it("should _push new SQS messages", function () {
+      var m = new mup.Manifest(true)
+        , msg = newSQSMsg(1).Messages[0]
+        ;
+
+      expect(m._push(msg)).to.equal(1);
+      expect(m.uris).to.deep.equal(msg._s3Event.s3URIs());
     });
 
-    it("should add an array of URIs", function () {
-      var m = new mup.Manifest(true);
-      m._push("uri0");
-      expect(m._addAll(["uri1","uri2", "uri3"])).to.equal(4);
-      expect(m.uris).to.deep.equal(["uri0", "uri1", "uri2", "uri3"]);
+    it("should return an array of SQS messages", function () {
+      var m = new mup.Manifest(true)
+        , msgs = newSQSMsg(5).Messages
+      ;
+      m._addAll(msgs);
+
+      expect(m.msgs).to.deep.equal(msgs);
+    });
+
+    it("should add an array of SQS messages", function () {
+      var m = new mup.Manifest(true)
+        , msgs = newSQSMsg(5).Messages
+        ;
+      m._push(msgs[0]);
+      expect(m._addAll(msgs.slice(1))).to.equal(5);
+
+      expect(m.uris).to.deep.equal(mup._toUris(msgs));
     });
 
     it("should generate correct-looking JSON", function () {
-      var m = new mup.Manifest(true);
-      m._push("uri1");
-      m._push("uri2");
-      expect(m.toJSON()).to.deep.equal({entries: [{url: "uri1", mandatory: true}, {url: "uri2", mandatory: true}]})
+      var m = new mup.Manifest(true)
+        , msgs = newSQSMsg(2).Messages
+      ;
+      m._addAll(msgs);
+      expect(m.toJSON()).to.deep.equal({entries: [{url: msgs[0]._s3Event.s3URIs()[0], mandatory: true}, {url: msgs[1]._s3Event.s3URIs()[0], mandatory: true}]})
     });
 
   });
 
+  describe("toUris", function () {
+    it("Should turn SQS messages to URIs", function () {
+      var msgs = newSQSMsg(2).Messages.concat([null, ""])
+        , uris = mup._toUris(msgs)
+        , uri1 = msgs[0]._s3Event.s3URIs()[0]
+        , uri2 = msgs[1]._s3Event.s3URIs()[0]
+        ;
+
+      expect(uris).to.have.length(2);
+      expect(uris[0]).to.equal(uri1);
+      expect(uris[1]).to.equal(uri2);
+
+
+    });
+  });
 
 });
