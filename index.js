f973f5f7ebf6d9fc8b6cb65acda97abea2b515f6
Tom Eklöf <tome@unity3d.com>
2015-02-19 12:14:37 -0500

    index.js changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
  * Created by teklof on 11.2.15.
  */
-exports.S3Copier = require('./lib/s3-to-rs').S3Copier;
-exports.Poller = require('./lib/sqs-poller').Poller;
+exports.copier = require('./lib/s3-to-rs');
+exports.poller = require('./lib/sqs-poller');
+exports.manifests = require('./lib/manifest-uploader');
